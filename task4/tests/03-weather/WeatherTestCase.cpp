a671f72a03d0dcd08e355b5584c1e37a1fe989f7
Ernest Nasyrov <2001092236@mail.ru>
2021-04-17 19:53:19 +0300

    Add 3 tests

--- task4/tests/03-weather/WeatherTestCase.cpp
+++ task4/tests/03-weather/WeatherTestCase.cpp
@@ -4,3 +4,21 @@
 
 #include "WeatherTestCase.h"
 #include "WeatherMock.h"
+
+using ::testing::Return;
+
+
+TEST_F(WeatherTestCase, GET)
+{
+    weather.Get("Moscow");
+}
+
+TEST_F(WeatherTestCase, GetTemperature)
+{
+    ASSERT_NO_THROW(weather.GetTemperature("Moscow"));
+}
+
+TEST_F(WeatherTestCase, SetApiKey)
+{
+    weather.SetApiKey("SUMMER");
+}
--- task4/tests/03-weather/WeatherTestCase.h
+++ task4/tests/03-weather/WeatherTestCase.h
@@ -2,9 +2,10 @@
 #pragma once
 
 #include <gtest/gtest.h>
+#include "WeatherMock.h"
+#include <cpr/cpr.h>
 
 class WeatherTestCase : public ::testing::Test {
+public:
+    WeatherMock weather;
 };
-
-
-
