b82ae8bbe7a8151421e9fdeacf2260841faa6cb2
Ernest Nasyrov <2001092236@mail.ru>
2021-05-15 20:08:43 +0300

    fixed

--- task4/tests/02-tree/TreeTestCase.cpp
+++ task4/tests/02-tree/TreeTestCase.cpp
@@ -9,32 +9,32 @@
 namespace bfs = boost::filesystem;
 
 TEST_F(TreeTestCase, GetTree) {
-    if (!boost::filesystem::exists("dir/subdir")) {
-        ASSERT_ANY_THROW(GetTree("bla-bla-bla", true));
-        boost::system::error_code ec;
-    }
 
-    if (!boost::filesystem::exists("dir/subdir")) {
-        boost::filesystem::create_directories("dir/subdir", ec);
+    ASSERT_ANY_THROW(GetTree("bla bla bla", true));
+    boost::system::error_code ec;
 
-        std::ofstream ofs("dir/file.txt");
-        GetTree("./dir", true);
+	
+    string ph = bfs::temp_directory_path().string() + "/" + bfs::unique_path().string()
+    boost::filesystem::create_directories(ph, ec);
 
-        GetTree("./dir", false);
-        ASSERT_ANY_THROW(GetTree("dir/file.txt", true));
-        boost::filesystem::remove_all("dir", ec);
-    }
+
+    GetTree(ph, true);
+
+    GetTree(ph, false);
+    ASSERT_ANY_THROW(GetTree(ph + "/file.txt", true));
+    boost::filesystem::remove_all(ph, ec);
 }
 
 TEST_F(TreeTestCase, FilterEmptyNodes) {
     boost::system::error_code ec;
 
-    boost::filesystem::create_directories("dir/subdir", ec);
-    std::ofstream ofs("dir/file.txt");
+    string ph = bfs::temp_directory_path().string() + "/" + bfs::unique_path().string()
+    boost::filesystem::create_directories(ph + "/subdir", ec);
+    std::ofstream ofs(ph + "/file.txt");
 
-    ASSERT_FALSE(GetTree("./dir", false) == GetTree("./dir", true));
+    ASSERT_FALSE(GetTree(ph, false) == GetTree(ph, true));
     ///FilterEmptyNodes(GetTree("./dir", false), ".");
-    FilterEmptyNodes(GetTree("./dir", false), "./dir");
+    FilterEmptyNodes(GetTree(ph, false), ph);
     ///FilterEmptyNodes(GetTree("./dir/file.txt", false), "./dir/file.txt");
-    boost::filesystem::remove_all("dir", ec);
+    boost::filesystem::remove_all(ph, ec);
 }
